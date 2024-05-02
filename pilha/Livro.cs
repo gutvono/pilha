28613737b9af64758f31d0a6505307c7cb332c2b
Gustavo <guvono4@gmail.com>
2024-05-02 10:01:10 -0300

    Add project files.

---
+++ pilha/Livro.cs
@@ -0,0 +1,19 @@
+﻿namespace pilha
+{
+    internal class Livro
+    {
+        string Titulo;
+        Livro Anterior;
+
+        public Livro(string titulo) 
+        {
+            this.Titulo1 = titulo;
+            this.Anterior1 = null;
+        }
+
+        public string Titulo1 { get => Titulo; set => Titulo = value; }
+        internal Livro Anterior1 { get => Anterior; set => Anterior = value; }
+
+
+    }
+}
---
+++ pilha/Pilha.cs
@@ -0,0 +1,42 @@
+﻿namespace pilha
+{
+    internal class Pilha
+    {
+        Livro topo;
+        int contador;
+
+        public Pilha() 
+        {
+            this.topo = null;
+            this.contador = 0;
+        }
+    
+        void push(Livro l)
+        {
+            //guarda o livro atual para nao perder referencia
+            Livro aux;
+            aux = this.topo;
+
+            //adiciona o novo livro no topo
+            this.topo = l;
+
+            //usa a variavel auxiliar para guardar a referencia do antigo livro que estava no topo
+            l.setAnterior(aux);
+
+            this.contador++;
+        }
+
+        Livro pop()
+        {
+            //auxiliar guarda o topo para nao perder a referencia
+            Livro aux;
+            aux = this.topo;
+
+            //topo agora é o que estava abaixo dele
+            topo = topo.GetAnterior();
+
+            //retorna o objeto que foi retirado
+            return aux;
+        }
+    }
+}
